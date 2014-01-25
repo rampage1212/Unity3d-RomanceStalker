3bddd4283513f24924dcd0614f8b46c31b4e5cb5
rampage0131 <jake.docker0131@gmail.com>
2014-01-25 10:06:05 +0700

    HideInto, camera follow

---
+++ UnityProject/Assets/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour 
+{
+	public GameObject ground;
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ground")
+			Instantiate(ground, new Vector3(transform.position.x, other.transform.position.y, transform.position.z), Quaternion.identity);
+	}
+}
