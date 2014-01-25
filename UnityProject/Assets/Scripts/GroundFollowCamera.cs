5bb911efb5cdd9918f11be366eb2a7eb217f3eb2
rampage0131 <jake.docker0131@gmail.com>
2014-01-25 11:04:24 +0700

    ground follow camera

---
+++ UnityProject/Assets/Scripts/GroundFollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundFollowCamera : MonoBehaviour {
+
+	public Camera camera;
+
+	float initXDistance;
+	Vector3 initPosition;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.Find ("Main Camera").camera;
+
+		initPosition = transform.position;
+		initXDistance = camera.transform.position.x - transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 targetPosition = initPosition;
+		targetPosition.x = camera.transform.position.x - initXDistance;
+
+		transform.position = targetPosition;
+	}
+}
